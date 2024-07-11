d366fe61e790aae3c44b26f977942adf65f98c0d
Aly <me@s5.pm>
2024-07-10 20:11:49 -0700

    Add some looping "error recovery"

--- NamedPipeRPCHost/NamedPipeRPCHost/Program.cs
+++ NamedPipeRPCHost/NamedPipeRPCHost/Program.cs
@@ -2,45 +2,84 @@
 
 const string pipeName = @"ytmusic-xiv-rpc";
 
-await using NamedPipeServerStream server = new(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
-await server.WaitForConnectionAsync();
+Task<bool> rwTask;
+Task<bool> wrTask;
 
-using CancellationTokenSource cts = new();
+do {
+    await using NamedPipeServerStream server = new(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+    await server.WaitForConnectionAsync();
 
-// TODO: if the named pipe errors, just loop, but if stdin/stdout close, exit
+    using CancellationTokenSource cts = new();
 
-await Task.WhenAll(
-    ReadWriteForever(cts, server),
-    WriteReadForever(cts, server)
-);
+    // if the named pipe errors, just loop, but if stdin/stdout close, exit
+    rwTask = ReadWriteForever(cts, server);
+    wrTask = WriteReadForever(cts, server);
+
+    await Task.WhenAll(
+        rwTask,
+        wrTask
+    );
+} while (rwTask.Result || wrTask.Result);
 
 return;
 
-async Task ReadWriteForever(CancellationTokenSource cts, NamedPipeServerStream writeDataTo) {
+async Task<bool> ReadWriteForever(CancellationTokenSource cts, NamedPipeServerStream writeDataTo) {
     await using (Stream stdin = Console.OpenStandardInput()) {
         byte[] chunk = new byte[4096];
         while (true) {
-            int count = await stdin.ReadAtLeastAsync(chunk, 1, false, cts.Token);
+            int count;
+            try {
+                count = await stdin.ReadAtLeastAsync(chunk, 1, false, cts.Token);
+            } catch (ObjectDisposedException ojde) {
+                return false;
+            }
+
             if (count == 0) {
                 await cts.CancelAsync();
-                return;
+                // stdin/stdout closed, so we shouldn't retry
+                return false;
             }
-            // We don't want this to cancel mid-way
-            await writeDataTo.WriteAsync(chunk.AsMemory(0, count));
+            
+            try {
+                // We don't want the write to the named pipe to cancel mid-way
+                await writeDataTo.WriteAsync(chunk.AsMemory(0, count));
+            } catch (IOException ioe) {
+                // error writing to named pipe so we should retry
+                return true;
+            } catch (ObjectDisposedException ojde) {
+                // the named pipe was disposed so we should retry
+                return true;
+            }
         }
     }
 }
 
-async Task WriteReadForever(CancellationTokenSource cts, NamedPipeServerStream readDataFrom) {
+async Task<bool> WriteReadForever(CancellationTokenSource cts, NamedPipeServerStream readDataFrom) {
     await using (Stream stdout = Console.OpenStandardOutput()) {
         byte[] chunk = new byte[4096];
         while (true) {
-            int count = await readDataFrom.ReadAtLeastAsync(chunk, 1, false, cts.Token);
-            if (count == 0) {
-                await cts.CancelAsync();
-                return;
+            int count;
+            try {
+                count = await readDataFrom.ReadAtLeastAsync(chunk, 1, false, cts.Token);
+            } catch (IOException ioe) {
+                // error reading from named pipe so we should retry
+                return true;
+            } catch (ObjectDisposedException ojde) {
+                // the named pipe was disposed so we should retry
+                return true;
             }
-            await stdout.WriteAsync(chunk.AsMemory(0, count), cts.Token);
+
+            if (count == 0) {
+                // EOF on named pipe so we should retry
+                await cts.CancelAsync();
+                return true;
+            }
+
+            try {
+                await stdout.WriteAsync(chunk.AsMemory(0, count), cts.Token);
+            } catch (ObjectDisposedException ojde) {
+                return false;
+            }
         }
     }
 }
